98985c6bc614a2468c4137923bb1f7d99fda2bdc
xnd-r <al.romanov2012@yandex.ru>
2020-04-04 17:22:32 +0300

    added random lower-triangular sparse matrix generator

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <random>
+
+void get_matrix(int n, std::vector<int>& val, std::vector<int>& col,std::vector<int>& row){
+    std::srand(unsigned(std::time(0)));
+    std::default_random_engine generator;
+    std::uniform_real_distribution<float> distribution(0., 1.);
+    std::vector<int> j_conunter;
+    int* i_conunter = new int[n]{0};
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < i; ++j){
+            if (distribution(generator) < 0.05f){
+                col.push_back(j);
+                i_conunter[i] += 1;
+                val.push_back(std::rand() % 5 + 1);
+            }
+        }
+        col.push_back(i);
+        i_conunter[i] += 1;
+        val.push_back(std::rand() % 5 + 1);
+    }
+    row.push_back(0);
+    for(int i = 1; i < n + 1; ++i){
+        row.push_back(i_conunter[i-1] + row[i-1]);
+    }
+    delete[] i_conunter;
+}
+
+
+int main(int* argc, char** argv){
+    int n = atoi(argv[1]);
+    std::vector<int> val; 
+    std::vector<int> col; 
+    std::vector<int> row; 
+    get_matrix(n, val, col, row);
+
+    for(std::vector<int>::iterator it = val.begin(); it != val.end(); ++it){
+        std::cout << *it << "\t";
+    }
+    std::cout <<"\n\n\n";
+    for(std::vector<int>::iterator it = col.begin(); it != col.end(); ++it){
+        std::cout << *it << "\t";
+    }
+    std::cout <<"\n\n\n";    
+    for(std::vector<int>::iterator it = row.begin(); it != row.end(); ++it){
+        std::cout << *it << "\t";
+    }
+    std::cout <<"\n\n\n" << val.size() << "\t" << col.size() << "\t" << row.size();    
+    return 0;
+}
+
