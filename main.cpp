4db2f7a675fba57bd120442a264a1dbd991e2095
xnd-r <al.romanov2012@yandex.ru>
2020-04-04 19:02:40 +0300

    started matrix vis

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <random>
 
-void get_matrix(int n, std::vector<int>& val, std::vector<int>& col,std::vector<int>& row){
+void get_matrix(int n, std::vector<int>& val, std::vector<int>& col, std::vector<int>& row){
     std::srand(unsigned(std::time(0)));
     std::default_random_engine generator;
     std::uniform_real_distribution<float> distribution(0., 1.);
@@ -29,6 +29,25 @@ void get_matrix(int n, std::vector<int>& val, std::vector<int>& col,std::vector<
     delete[] i_conunter;
 }
 
+void print_matrix(int n, std::vector<int>& val, std::vector<int>& col, std::vector<int>& row){
+   for (int i = 0; i < n; ++i){
+        for (int j = 0; j < n; ++i){
+            for (int ir = 0; ir < row.size()-1; ++ir){
+                int start = row[ir];
+                int finish = row[ir+1];
+                for (int k = start; j < finish; ++k){
+                    if (j != col[row[k]]){
+                        std::cout << "0" << "\t";
+                    }
+                    else{
+                        std::cout << val[row[k]] << "\t";
+                    }
+                }
+            }
+        }
+        std::cout << "\n";
+    } 
+}
 
 int main(int* argc, char** argv){
     int n = atoi(argv[1]);
@@ -36,6 +55,7 @@ int main(int* argc, char** argv){
     std::vector<int> col; 
     std::vector<int> row; 
     get_matrix(n, val, col, row);
+    print_matrix(n, val, col, row);
 
     for(std::vector<int>::iterator it = val.begin(); it != val.end(); ++it){
         std::cout << *it << "\t";
